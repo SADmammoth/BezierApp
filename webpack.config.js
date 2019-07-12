ed2c1b603b756ff1f1e45741b4ff44269cf3cf58
SADmammoth <lo.ma200018@gmail.com>
2019-07-12 19:50:22 +0300

    init: configs

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: __dirname + '/dist',
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, loader: 'eslint-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(otf|png|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          'html-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+}
